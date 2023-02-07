429c02cddc96ebe7ba38004d08352538fc8eec3e
sdairs <sdairs@users.noreply.github.com>
2023-02-07 21:33:55 +0000

    missed from 2d game commit

--- godot/game.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-func _ready():
-	var logger = self.get_node("logger")
-	logger.log('INFO', logger.event_types.LOAD_SCENE, {'scene_name': str(self.name)})
-	pass
-
-func _process(_delta):
-	pass
